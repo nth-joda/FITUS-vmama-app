de448e01658f7df89656fc0338470cbe6e8eb474
NTHau <nthau18@apcs.fitus.edu.vn>
2022-05-05 15:46:31 +0700

    responsive for UI:login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Auth from "./components/auth/Auth";
 import './App.css';
+// import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <Auth />
+      {/* <Dashboard /> */}
     </div>
   );
 }
---
+++ src/components/dashboard/dashboardHeader/DashboardHeader.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./dashboardHeader.css"
+
+const DashboardHeader = () => {
+  return (
+    <div className="dashboardHeader">DashboardHeader</div>
+  )
+}
+
+export default DashboardHeader
