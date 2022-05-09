a7c95828c67d98853469d5da5e9861b88b177aa4
NTHau <nthau18@apcs.fitus.edu.vn>
2022-05-09 14:05:56 +0700

    load children by nav

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import Auth from '../auth/Auth';
 import "./dashboard.css"
 import DashboardHeader from './dashboardHeader/DashboardHeader'
 import LeftNav from './leftNav/LeftNav';
 import Vouchers from "./vouchers/Vouchers";
 
 const Dashboard = () => {
+  const [location, setLocation] = useState("dashboard");
+  
   return (
     <div>
       <DashboardHeader/>
       <div className="dashboard_content">
-        <LeftNav className="dashboard__leftNav" />
+        <LeftNav className="dashboard__leftNav" changeContent={(loc) => setLocation(loc)}  />
         <div className="dashboard__container">
-          <Vouchers />
+          {(location === "vouchers" || location === "dashboard") && <Vouchers />}
+          {location === "policy" && <p>Policy</p>}
+          {location === "product" && <p>Product</p>}
+          {location === "logout" && <p>logout</p>}
         </div>
       </div>
 
--- src/components/dashboard/leftNav/LeftNav.jsx
+++ src/components/dashboard/leftNav/LeftNav.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import "./leftNav.css";
 
 import NavOption from "./navOption/NavOption"
 
-const LeftNav = () => {
+const LeftNav = (props) => {
+    const [beingActive, setBeingActive] = useState("vouchers");
+
+    const setChange = (loc) => {
+        props.changeContent(loc);
+        setBeingActive(loc);
+    }
     return (
         <div className="leftNav">
-            <NavOption icon={"voucher"} dpName="Vouchers" />
-            <NavOption icon={"policy"} dpName="Policy" />
-            <NavOption icon={"product"} dpName="Product" />
-            <NavOption icon={"policy"} dpName="Policy" />
+            <div className={beingActive === "vouchers" ? "nav__item active" : "nav__item" } id="nav__vouchers" onClick={() => setChange("vouchers")}>
+                <NavOption key="vouchers" icon={"voucher"} dpName="Vouchers" />
+            </div>
+            <div className={beingActive === "policy" ? "nav__item active" : "nav__item" } id="nav__policy" onClick={() =>  setChange("policy")}>
+                <NavOption key="policy" icon={"policy"} dpName="Policy"  />
+            </div>
+            <div className={beingActive === "product" ? "nav__item active" : "nav__item" } id="nav__product" onClick={() =>  setChange("product")}>
+                <NavOption key="product" icon={"product"} dpName="Product" />
+            </div>
+            
         </div>
     )
 }
